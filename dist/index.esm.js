716618eaecfca06871c636ab834ce34604611b6d
王歧伟 <wangqiwei.noly@bytedance.com>
2022-10-08 12:36:08 +0800

    gogogo

---
+++ dist/index.esm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+var index = (function (_ref) {
+  var title = _ref.title;
+  return /*#__PURE__*/React.createElement("h1", null, title);
+});
+
+export { index as Foo };
